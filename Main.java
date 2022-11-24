d630ab794dcbdc847c85ffe3d56338aa76c1fbba
FilipeHSAraujo <84210747+FilipeHSAraujo@users.noreply.github.com>
2022-11-24 12:08:20 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 	//variables
-		double area, ray, pi = 3.14159;
+		int x, y;
 	
 		Scanner sc = new Scanner(System.in);
 		
 	//Program
-		System.out.println("Calculadora para Area do Circulo!!!!!!");
-		System.out.print("Digite o valor do Raio: ");
-		ray = sc.nextDouble();
-		area = pi * (Math.pow(ray,2));
-		System.out.printf("Que legal, o raio do seu circulo e %.4f!!!", area);
+		System.out.println("Calculadora para soma!!!!!!");
+		System.out.print("Digite o primeiro numero: ");
+		x = sc.nextInt();
+		System.out.print("Digite o Segundo numero: ");
+		y = sc.nextInt();
+		int soma = x+y;		
+		System.out.printf("Que legal, a sua soma deu %d!!!", soma);
 
 	}
 
