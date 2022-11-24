2a0273ab9d7836c72631c1207fce4e47fff9155d
FilipeHSAraujo <84210747+FilipeHSAraujo@users.noreply.github.com>
2022-11-24 10:50:04 -0300

    Task1: Sum Task
    Hi there!!! I did this console app for my first step on Java.
    This app only works with INT number, and I hope you guys enjoy my baby steps 😎🐱‍💻.

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	//variables
+		int x, y;
+	
+		Scanner sc = new Scanner(System.in);
+		
+	//Program
+		System.out.println("Calculadora para soma!!!!!!");
+		System.out.print("Digite o primeiro numero: ");
+		x = sc.nextInt();
+		System.out.print("Digite o Segundo numero: ");
+		y = sc.nextInt();
+		int soma = x+y;		
+		System.out.printf("Que legal, a sua soma deu %d!!!", soma);
+
+	}
+
+}
